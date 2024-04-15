0dccc990a9ff3ee8d337443cea66d14696ce5c6d
ThuVien_API <triminhh68@email.com>
2024-04-15 14:43:17 +0700

    ThuVien_API

---
+++ ThuVien_AIP/Data/DbInitializer.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ThuVien_AIP.Models;
+
+namespace ThuVien_AIP.Data
+{
+    public class DbInitializer
+    {
+        private readonly ModelBuilder _builder;
+
+        public DbInitializer(ModelBuilder builder)
+        {
+            _builder = builder;
+        }
+
+        public void Seed()
+        {
+
+            _builder.Entity<BookAuthor>(b =>
+            {
+                b.HasData(new Author
+                {
+                    // Use "Id" instead of "id"
+                    FullName = "Harry Potter and the Sorcerer's Stone",
+                });
+
+                b.HasData(new Author
+                {
+                    // Use "Id" instead of "id"
+                    FullName = "Harry Potter and the Chamber of Secrets",
+                });
+
+            });
+    }
+    }
+}
+
